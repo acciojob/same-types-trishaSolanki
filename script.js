4c4e1ad810d9bc0388e1e8b8cd342e11336d6158
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-20 08:43:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function isSameType(value1, value2) {
   //your js code here
+	isSameType(1, 3); // true
+isSameType("hey", "hello"); // true
+isSameType(NaN, NaN); // true
+isSameType("3", 3); // false
 }
 
 // do not change the code below.
